48592856f822d20c5041775c738514c9399ee300
Thecoder3281F <happymanjarrett@outlook.com>
2023-05-29 03:06:38 +0000

    Hi this is my first commit of the Flask AI server

--- flask_app.py
+++ flask_app.py
@@ -1,1 +1,36 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+from pathlib import Path
+from flask import Flask, request
+import tensorflow as tf
 
+from clean_text import clean_texts
+
+
+CWD = Path(__file__).parent.resolve()
+
+model = tf.keras.models.load_model(CWD / 'models/cyberbullying-bdlstm.h5')
+
+with open(CWD / 'models/tokenizer.json', encoding='UTF-8') as file:
+    tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(file.read())
+
+app = Flask(__name__)
+
+@app.route('/cyberbullyingchatbotmsg', methods=['GET'])
+def cyberbullyingchatbotmsg():
+    messages = clean_texts([request.args.get("msg")], tokenizer)
+    return {"score": model.predict(messages).tolist()[0][0]}
+
+@app.route('/', methods=['GET'])
+def main():
+    return "Hello World"
+
+@app.route('/echo', methods=['GET', 'POST'])
+def echo():
+    if 'echo' in request.args:
+        return request.args.get('echo')
+    else:
+        return "Param not found", 404
+
+app.run(debug=True)
